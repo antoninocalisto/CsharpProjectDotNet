daaebc04ea211740f30359f5d1c1e2a7476cd1b9
antoninocalisto <antuninosantos@gmail.com>
2021-12-14 09:31:36 -0300

    [Antonino Calisto] - Criação do Projeto - Projeto básico

---
+++ CsharpProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
